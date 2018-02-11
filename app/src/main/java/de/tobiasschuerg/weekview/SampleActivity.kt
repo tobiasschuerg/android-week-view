d0224a14aad37a64434f86619c83c5a246a5b046
Tobias Schürg <tobiasschuerg@gmail.com>
2018-02-11 11:22:12 +0100

    created initial project

---
+++ app/src/main/java/de/tobiasschuerg/weekview/SampleActivity.kt
@@ -0,0 +1,12 @@
+package de.tobiasschuerg.weekview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
