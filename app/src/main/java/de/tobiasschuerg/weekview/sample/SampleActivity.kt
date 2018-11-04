6c35c37e9f73e3ebe2f9dc5727ecd2eafa04c6d4
Tobias Schürg <tobiasschuerg@gmail.com>
2018-11-04 22:31:24 +0100

    Migrated to AndroidX.

--- app/src/main/java/de/tobiasschuerg/weekview/sample/SampleActivity.kt
+++ app/src/main/java/de/tobiasschuerg/weekview/sample/SampleActivity.kt
@@ -2,7 +2,7 @@ package de.tobiasschuerg.weekview.sample
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.ContextMenu
 import android.view.Menu
--- library/src/main/java/de/tobiasschuerg/weekview/view/WeekView.kt
+++ library/src/main/java/de/tobiasschuerg/weekview/view/WeekView.kt
@@ -3,7 +3,7 @@ package de.tobiasschuerg.weekview.view
 import android.content.Context
 import android.graphics.Color
 import android.os.Build
-import android.support.annotation.RequiresApi
+import androidx.annotation.RequiresApi
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
