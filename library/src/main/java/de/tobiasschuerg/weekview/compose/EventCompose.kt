15e26ee3df65dfe95fc454b5b480a189a950448e
Tobias Schürg <tobiasschuerg@gmail.com>
2025-08-31 23:16:45 +0200

    chore: Optimize imports

--- library/src/main/java/de/tobiasschuerg/weekview/compose/EventCompose.kt
+++ library/src/main/java/de/tobiasschuerg/weekview/compose/EventCompose.kt
@@ -2,7 +2,14 @@ package de.tobiasschuerg.weekview.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
